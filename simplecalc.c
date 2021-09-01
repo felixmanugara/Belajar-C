6c5ea6e84f711ef8abd810f4bfb5bd7521160dd1
Felix Manugara <marcofel.manugara@gmail.com>
2021-09-01 18:59:42 +0700

    membuat kalkulator sederhana

---
+++ simplecalc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int num1;
+    int num2;
+    printf("masukkan angka pertama: ");
+    scanf("%d", &num1);
+    printf("masukkan angka kedua: ");
+    scanf("%d", &num2);
+
+    printf("jawaban: %d", num1 + num2);
+}
