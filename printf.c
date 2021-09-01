43e1ddc561b4ff3d03721c9f336ceadf6474519b
Felix Manugara <marcofel.manugara@gmail.com>
2021-09-01 18:55:09 +0700

    tentang printf function

---
+++ printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+
+    printf("halo\n");
+    // kita dapat menggunakan fungsi printf untuk mencetak nilai
+    // berupa string atau integer secara langsung dengan identifier %
+    printf("%d\n",100);
+    printf("%s\n","ini adalah printf untuk string");
+    printf("nama saya %s\nsaya berumur %d tahun\n","Felix",21);
+
+    return 0;
+    
+}
