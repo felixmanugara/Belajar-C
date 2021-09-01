30d1c9e80e3b90bc0e947526fd7c6fc752661523
Felix Manugara <marcofel.manugara@gmail.com>
2021-09-01 18:54:11 +0700

    berbagai macam tipe data dalam bahasa C

---
+++ tipe_data.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+
+    int umur = 21; 
+    double ip = 3.8;
+    float trans = 565633.7
+    char grade = 'A'; // char hanya untuk menyimpan 1 karakter
+    char kata[] = "semester genap"; // char[] untuk string
+
+    return 0;
+    
+}
