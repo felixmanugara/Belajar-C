09dff9a4eba993a5d7cb8b6ce744e161362398e9
Felix Manugara <marcofel.manugara@gmail.com>
2021-09-01 18:57:00 +0700

    tentang operasi aritmatika

---
+++ numbers_operations.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int num_1 = 3;
+    int num_2 = 6;
+    printf("3 x 6 = %d\n",num_1 * num_2); /* operasi lain +, -, */
+    printf("5^8 = %f\n", pow(5, 8)); /* pow() adalah fungsi pemangkatan */
+    printf("akar dari 36 adalah %f\n",sqrt(36)); /* sqrt() adalah fungsi akar */
+
+    return 0;
+}
+
