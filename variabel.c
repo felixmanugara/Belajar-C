7d6b8c001c724d2c1d58996274d6a443c1d5efdc
Felix Manugara <marcofel.manugara@gmail.com>
2021-09-01 18:52:57 +0700

    tentang variabel dalam bahasa C

---
+++ variabel.c
@@ -0,0 +1,19 @@
+// include dilakukan untuk memasukkan library ke dalam program c
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// int main() adalah method dimana program c ditulis
+int main() {
+    // dalam c membuat variabel dibutuhkan deklarasi tipe data
+    char name[] = "felix";
+    int age = 21;
+    // printf digunakan untuk output ke pengguna
+    printf("Hello world! i'm %s\n",name); // %s merupakan identifier untuk tipe data string
+    printf("saya berumur %d\n",age); // %d merupakan identifier untuk tipe data integer
+    return 0;
+}
+
+
+
+
