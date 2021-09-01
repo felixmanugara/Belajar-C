0af53ce818e7b8daf4685b4c918e2ee3065d8923
Felix Manugara <marcofel.manugara@gmail.com>
2021-09-01 19:00:40 +0700

    array dalam bahasa C

---
+++ array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* array adalah container yang
+digunakan untuk menyimpan data 
+dalam jumlah banyak */
+int main() {
+    // tanda [] digunakan untuk membuat array
+    int number[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    printf("%d", number[5]);
+    /* angka 5 di dalam number[] menunjukan indeks
+    dari data yang akan diakses oleh fungsi printf() */
+
+    return 0;
+
+}
